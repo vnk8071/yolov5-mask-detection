8df53445182a9e53d3f9599d7586b74625a8fb7d
vnk8071 <nguyenkhoi8071@gmail.com>
2022-01-25 14:36:23 +0700

    ADD download data script

---
+++ setup_data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python ./utils/get_data.py
+rm ./data/mask-fpt-ai.zip
---
+++ utils/get_data.py
@@ -0,0 +1,54 @@
+import os
+import shutil
+import requests
+from zipfile import ZipFile
+
+FILE_ID = '1-yPhAZ6tnTjh7PY2nNzkg2AVWfzgIRmN'
+TARGET_PATH = 'data/mask-fpt-ai.zip'
+
+
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params={'id': id}, stream=True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = {'id': id, 'confirm': token}
+        response = session.get(URL, params=params, stream=True)
+
+    save_response_content(response, destination)
+
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+
+def save_response_content(response, target_path):
+    CHUNK_SIZE = 32768
+
+    with open(target_path, "wb") as f:
+        TOTAL_LENGTH = int(response.headers.get('content-length'))
+        print("Total size: ", TOTAL_LENGTH)
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+
+
+if __name__ == '__main__':
+    print('Start download data')
+    if os.path.exists('./data'):
+        shutil.rmtree('./data')
+    os.makedirs('./data')
+    download_file_from_google_drive(FILE_ID, TARGET_PATH)
+    print('Download done')
+
+    with ZipFile('./data/mask-fpt-ai.zip', 'r') as zip_file:
+        zip_file.extractall('./data')
+    print('Success unzip file')
