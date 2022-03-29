93e9112d0331f7dafaa0cfac4a85a04295738bff
vnk8071 <nguyenkhoi8071@gmail.com>
2022-03-29 22:26:03 +0700

    UPDATE setup_data

--- setup_data.sh
+++ setup_data.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-python ./utils/get_data.py
-rm ./data/mask-fpt-ai.zip
+echo 'Downloading and setting up data'
+DEST_DIR='data' 
+FILENAME='mask-fpt-ai.zip'
+
+mkdir $DEST_DIR
+gdown https://drive.google.com/uc?id=1D1_lUIucMWqSDqZokohl8Aqd2G5K-LIz
+mv $FILENAME $DEST_DIR
+unzip "${DEST_DIR}/${FILENAME}" -d $DEST_DIR
+rm "${DEST_DIR}/${FILENAME}"
+echo 'Done'
